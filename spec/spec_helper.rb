eae4e07e0ffbeb6ab8fea7a08028a4271c31c917
Nick Veys <nick@codelever.com>
2014-08-07 16:55:28 -0500

    Adding reference file, example file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,16 +21,11 @@ RSpec.configure do |config|
 end
 
 def data_file(name)
-  File.expand_path("#{File.dirname(__FILE__)}/support/data/#{name}")
-end
-
-def data_files
-  dir = "#{File.dirname(__FILE__)}/support/data/#{dir}"
-  Dir.glob("#{dir}/*").select { |e| File.file? e }
+  File.expand_path("#{File.dirname(__FILE__)}/../support/data/#{name}")
 end
 
 def invalid_data_files
-  dir = "#{File.dirname(__FILE__)}/support/data/invalid"
+  dir = "#{File.dirname(__FILE__)}/../support/data/invalid"
   invalid = Dir.glob("#{dir}/**/*").select { |e| File.file? e }
   invalid << __FILE__
   invalid << 'not-really-a-file.resources'
---
+++ support/cs-dump-resources/DumpResources.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Resources;
+
+/// Build via 'mcs DumpResources.cs'
+public class DumpResources
+{
+   public static void Main()
+   {
+        ResourceReader rdr = new ResourceReader("Example.resources");
+        Console.WriteLine(rdr.GetType());
+        IDictionaryEnumerator dict = rdr.GetEnumerator();
+        while (dict.MoveNext()) {
+            Console.WriteLine("{0}: '{1}' (Type {2})",
+                dict.Key, dict.Value, dict.Value.GetType().Name);
+        }
+        rdr.Close();
+   }
+}
