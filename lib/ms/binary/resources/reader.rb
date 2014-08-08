ccc2d0bf70e908a7dba888aa697c3ff66305ecbb
Nick Veys <nick@codelever.com>
2014-08-08 10:38:23 -0500

    Adding resource types

---
+++ lib/ms/binary/resources/reader.rb
@@ -0,0 +1,5 @@
+module Ms
+  module BinaryResources
+
+  end
+end
---
+++ lib/ms/binary/resources/resource_types.rb
@@ -0,0 +1,28 @@
+module Ms
+  module BinaryResources
+
+    RESOURCE_TYPES = {
+      null: 0,
+      string: 1,
+      bool: 2,
+      char: 3,
+      byte: 4,
+      sbyte: 5,
+      int16: 6,
+      uint16: 7,
+      int32: 8,
+      uint32: 9,
+      int64: 10,
+      uint64: 11,
+      single: 12,
+      double: 13,
+      decimal: 14,
+      datetime: 15,
+      timespan: 16,
+      bytearray: 32,
+      stream: 33,
+      fistcustom: 64,
+    }
+
+  end
+end
