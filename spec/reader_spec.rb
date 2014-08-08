44d43293281422859f20cc821de14f1dcced09f5
Nick Veys <nick@codelever.com>
2014-08-08 13:44:15 -0500

    Specs for string reading

--- spec/reader_spec.rb
+++ spec/reader_spec.rb
@@ -18,4 +18,46 @@ describe Ms::BinaryResources::Reader do
 
   its(:type_count) { should eql(0) }
 
+  describe '#key?' do
+
+    %w(Title Label1 Label2 Label3 Label4 Label5 Label6 Label7 Label8
+       Label9 Label10 Label11 Label12 Label13 Label14 Label15).each do |key|
+      it "is true for #{key}" do
+        expect(subject.key?(key)).to be_truthy
+      end
+    end
+
+    it 'is false for keys not present' do
+      expect(subject.key?('hello')).to be_falsey
+    end
+
+  end
+
+  describe '#[]' do
+
+    {
+      'Title' => '"Contact Information"',
+      'Label1' => '"First Name:"',
+      'Label2' => '"Middle Name:"',
+      'Label3' => '"Last Name:"',
+      'Label4' => '"SSN:"',
+      'Label5' => '"Street Address:"',
+      'Label6' => '"City:"',
+      'Label7' => '"State:"',
+      'Label8' => '"Zip Code:"',
+      'Label9' => '"Home Phone:"',
+      'Label10' => '"Business Phone:"',
+      'Label11' => '"Mobile Phone:"',
+      'Label12' => '"Other Phone:"',
+      'Label13' => '"Fax:"',
+      'Label14' => '"Email Address:"',
+      'Label15' => '"Alternate Email Address:"',
+    }.each do |key, value|
+      it "is #{value} for #{key}" do
+        expect(subject[key]).to eql(value)
+      end
+    end
+
+  end
+
 end
